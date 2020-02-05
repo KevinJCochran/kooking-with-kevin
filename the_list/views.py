615a1872d1d7d120c3b669e6766fa4d364ad8f66
Kevin Cochran <kevinjcochran@gmail.com>
2020-02-04 23:55:10 -0500

    Add login page

--- the_list/views.py
+++ the_list/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.http import HttpResponseRedirect, HttpResponseForbidden
+from django.shortcuts import render, reverse
 from .models import ListItem
+
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
 def index(request):
     the_list = ListItem.objects.all()
     return render(request, 'the_list/index.html', {'the_list': the_list})
+
+
+def start_session(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse('the_list:index'))
+    else:
+        return HttpResponseForbidden()
 
 
 # Render the add item page
